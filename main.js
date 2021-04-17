f28311ef19aef0b538fc4a738d8a6747c4799ca1
kost12-bit <72891196+kost12-bit@users.noreply.github.com>
2021-04-17 11:51:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+let map = {
+    x: 0,
+    y: 0,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    color: 'red',
+    border: '#f1f1f1'
+}
+
+let cube = {
+    id: 'cube',
+    x: 0,
+    y: 0,
+    width: 56,
+    height: 56,
+    trnstn: .25,
+    step: 56,
+    color: 'black'
+}
+
+let bots = [];
+
+let bot = {
+    id: 1,
+    x: 0,
+    y: 0,
+    width: 56,
+    height: 56,
+    trnstn: .25,
+    step: 56,
+    color: 'yellow'
+}
+
+bots.push(bot);
+
+let buffs = [];
+
+let buff = {
+    id: 1,
+    x: 200,
+    y: 200,
+    width: 56,
+    height: 56,
+    trnstn: .25,
+    color: 'purple'
+}
+
+buffs.push(buff);
+
+renderMap(map);
+renderCube(spawnCube(map, cube));
+renderBots(bots);
+renderBuffs(buffs);
